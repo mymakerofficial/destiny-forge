b06eba660de7de49cc07067093b67d8c684031e3
My_Maker <36822242+mymakerofficial@users.noreply.github.com>
2025-01-13 16:26:20 +0100

    chore: add shadcn

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+</script>
 
 <template>
   <main>
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
+    <Button>Test</Button>
   </main>
 </template>
